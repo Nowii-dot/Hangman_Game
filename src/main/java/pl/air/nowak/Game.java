129f7a9d94348948ebef7437bb10c377d8bf82e2
Sebastian Nowak <nowaksebastian998@gmail.com>
2023-06-06 21:50:21 +0200

    Dodanie Kategorii

--- src/main/java/pl/air/nowak/Game.java
+++ src/main/java/pl/air/nowak/Game.java
@@ -1,54 +1,82 @@
 package pl.air.nowak;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+import java.lang.Character;
 
 public class Game {
 
-    private List<String> cars = List.of("bwm", "audi", "wheel", "fuel", "mercedes");
+    private List<String> Cars = List.of("bwm", "audi", "wheel", "fuel", "mercedes");
+    private List<String> Animal = List.of("dog", "cat", "lion", "owl", "duck");
+    private List<String> Profession = List.of("miner", "firefighter", "doctor");
+
+
     private String word;
     private int hp = 3;
-
-
-
     private char[] userWord;
 
-    public void PlayGame(){
+    //funkcja obsługująca całą grę
+    public void ChooseCategory(){
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Choose Category");
+            System.out.println("1. Cars");
+            System.out.println("2. Animal");
+            System.out.println("3. Profession");
+            System.out.println("4. End Game");
+
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+                case 1 -> PlayGame(Cars);
+                case 2 -> PlayGame(Animal);
+                case 3 -> PlayGame(Profession);
+                case 4 -> {
+                    scanner.close();
+                    System.exit(0);
+                }
+            }
+
+        }
+    }
+    public void PlayGame(List NameCategory){
         Scanner scanner = new Scanner(System.in);
         Random randomWord = new Random();
-        word  = cars.get(randomWord.nextInt(cars.size()));
+
+
+        word  = NameCategory.get(randomWord.nextInt(NameCategory.size())).toString();
+        System.out.println(word);
 
         userWord = new char[word.length()];
         Arrays.fill(userWord, '_');
 
         while (!gameEnded())
         {
-            System.out.print(userWord);
-            System.out.print(word.length());
-            System.out.println("twoje hp: " + hp);
+            System.out.println(userWord);
+            System.out.println("Word lenght: "+ word.length());
+            System.out.println("Your Hp: " + hp);
             System.out.println();
-            System.out.println("Podaj literę");
-            System.out.println();
+            System.out.print("Enter the letter:");
             char letter = scanner.nextLine().charAt(0);
+            char lowLetter = Character.toLowerCase(letter);
 
 
-            checkLetter(letter);
+            checkLetter(lowLetter);
             gameEnded();
 
         }
         if(hp == 0){
             System.out.println("Game Over :(");
+            System.out.println(word);
         }
         else
         {
             System.out.println("Congratulations you won :)");
         }
 
-        scanner.close();
+
     }
 
+    //funkcja sprawdzająca poprawność literki w słowie
     private void checkLetter(char letter) {
 
         boolean foundLetter = false;
